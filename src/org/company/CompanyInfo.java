b3116c51707d174cb9c7ab0466a7887adb9df9d0
aravind1599 <aravind15899@gmail.com>
2021-06-30 15:55:21 +0530

    Git model2 testing

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,32 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName(String name) {
+		System.out.println("CompanyName:"+name);
+		
+
+	}
+	private void companyName(String name1,int id) {
+		System.out.println("CompanyName1:"+name1+id);
+		
+
+	}
+	private void companyName(String name,int id,String city) {
+		System.out.println("CompanyName2:"+name+id+city);
+		
+
+	}
+	private void companyName(String name,int id,String city,long pin) {
+		System.out.println("CompanyName3:"+name+id+city+pin);
+		
+
+	}
+	public static void main(String[] args) {
+		CompanyInfo c=new CompanyInfo();
+		c.companyName("google");
+		c.companyName("google", 88);
+		c.companyName("google", 88, "Noida");
+		c.companyName("google", 88, "Noida", 489320l);
+	}
+
+}
